51ef742615eb78417a9248dad7651997c92e8691
Takahiro Morita <taro@Takahiros-MacBook-Air.local>
2017-03-26 12:53:28 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Movie Trailer Website
+
+This project is part of the Full Stack Web Developer nanodegree from udacity.
+The project utilizes [Programming Foundations with Python](https://www.udacity.com/course/programming-foundations-with-python--ud036)
+
+##Usage
+Access [fresh_tomatoes.html]()
+
+## The purpose of this project
+Sever-side code to store a list of my favorite movies, including box art imagery and a movie trailer URL. Using my code to generate a static web page, visitors are allowed to browse their movies and watch the trailers.
+
+## Code Quality
+[Here](https://google.github.io/styleguide/pyguide.html) is the Google Python Style Guide that I followed.
---
+++ entertainment_center.py
@@ -0,0 +1,37 @@
+import fresh_tomatoes
+import media
+
+kung_fu_panda = media.Movie("Kung Fu Panda",
+"A panda who learned Kung Fu",
+"https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Kungfupanda.jpg/220px-Kungfupanda.jpg",
+"https://www.youtube.com/watch?v=PXi3Mv6KMzY",
+)
+
+jurassic_park = media.Movie("Jurassic Park ",
+"Dinasours in an island",
+"https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg",
+"https://www.youtube.com/watch?v=Bim7RtKXv90")
+
+mask = media.Movie("The Mask",
+"A guy got a magical mask",
+"https://upload.wikimedia.org/wikipedia/en/thumb/4/4b/The_Mask_%28film%29_poster.jpg/220px-The_Mask_%28film%29_poster.jpg",
+"https://youtu.be/hOqVRwGVUkA")
+
+
+lion_king = media.Movie("The Lion King",
+"King of lion in Savanna",
+"https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg",
+"https://www.youtube.com/watch?v=4sj1MT05lAA")
+
+ghostbusters = media.Movie("Ghostbusters",
+"Guys buster the bunch of ghosts",
+"https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Ghostbusters_2016_film_poster.png/220px-Ghostbusters_2016_film_poster.png",
+"https://www.youtube.com/watch?v=w3ugHP-yZXw")
+
+ip_man = media.Movie("Ip Man",
+"The father of Wing Chun",
+"https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/Ipmanposter02.jpg/220px-Ipmanposter02.jpg",
+"https://www.youtube.com/watch?v=1AJxXQ7xojE")
+
+movies = [kung_fu_panda, jurassic_park, mask, lion_king, ghostbusters, ip_man]
+fresh_tomatoes.open_movies_page(movies)
