34fd13e070d777d1dec34dc27469b6399b8f5704
Takahiro Morita <taro@Takahiros-MacBook-Air.local>
2017-03-28 06:28:19 +0700

    Add comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,11 @@
 import fresh_tomatoes
 import media
-
+# Save each movie information in the memory
+# Use Movie class from media.py file to create multiple instances
 kung_fu_panda = media.Movie("Kung Fu Panda",
 "A panda who learned Kung Fu",
-"https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Kungfupanda.jpg/220px-Kungfupanda.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Kungfupanda.jpg/220px-K\
+ungfupanda.jpg",
 "https://www.youtube.com/watch?v=PXi3Mv6KMzY",
 )
 
@@ -14,24 +16,31 @@ jurassic_park = media.Movie("Jurassic Park ",
 
 mask = media.Movie("The Mask",
 "A guy got a magical mask",
-"https://upload.wikimedia.org/wikipedia/en/thumb/4/4b/The_Mask_%28film%29_poster.jpg/220px-The_Mask_%28film%29_poster.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/4/4b/The_Mask_%28film%29_pos\
+ter.jpg/220px-The_Mask_%28film%29_poster.jpg",
 "https://youtu.be/hOqVRwGVUkA")
 
 
 lion_king = media.Movie("The Lion King",
 "King of lion in Savanna",
-"https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg\
+/220px-The_Lion_King_poster.jpg",
 "https://www.youtube.com/watch?v=4sj1MT05lAA")
 
 ghostbusters = media.Movie("Ghostbusters",
 "Guys buster the bunch of ghosts",
-"https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Ghostbusters_2016_film_poster.png/220px-Ghostbusters_2016_film_poster.png",
+"https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Ghostbusters_2016_film_p\
+oster.png/220px-Ghostbusters_2016_film_poster.png",
 "https://www.youtube.com/watch?v=w3ugHP-yZXw")
 
 ip_man = media.Movie("Ip Man",
 "The father of Wing Chun",
-"https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/Ipmanposter02.jpg/220px-Ipmanposter02.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/Ipmanposter02.jpg/220px\
+-Ipmanposter02.jpg",
 "https://www.youtube.com/watch?v=1AJxXQ7xojE")
 
+# Store the movies in array
 movies = [kung_fu_panda, jurassic_park, mask, lion_king, ghostbusters, ip_man]
+
+# Fire open_movies_page function with movies argument from fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,11 +1,18 @@
 import webbrowser
-
+# Create Movie class to create multiple instances
 class Movie():
     """ This class provides a way to store movie related information"""
-    
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    # Initialize and create space in memory with defining __init__
+    # self is the object being created. Running called kung_fu_panda, the __init__
+    # Initialize these title, storyline, image trailer as arguments to __init__
+    # function.
+    # function inside the class Movie get called.
+    def __init__(self, movie_title, movie_storyline, poster_image,
+    trailer_youtube):
+        # Remember several information inside our class
+        # Initialize varialbes
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
