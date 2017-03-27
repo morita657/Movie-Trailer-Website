844e78b14ac3a86cadaf833ed20635c781313876
Takahiro Morita <taro@Takahiros-MacBook-Air.local>
2017-03-28 06:35:09 +0700

    Edit comments

--- media.py
+++ media.py
@@ -4,11 +4,12 @@ class Movie():
     """ This class provides a way to store movie related information"""
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    # Initialize and create space in memory with defining __init__
-    # self is the object being created. Running called kung_fu_panda, the __init__
-    # Initialize these title, storyline, image trailer as arguments to __init__
-    # function.
-    # function inside the class Movie get called.
+    # Initialize and create space in memory with defining __init__.
+    # self is the object being created.
+    # Running called kung_fu_panda, the __init__ function initializes
+    # these title, storyline, image trailer as arguments inside
+    # __init__ function.
+    
     def __init__(self, movie_title, movie_storyline, poster_image,
     trailer_youtube):
         # Remember several information inside our class
